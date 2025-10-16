9e2c208dad6ad3bbc4dd319fc7d7470a05d417ec
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2025-10-16 00:42:04 +0000

    chore: change next pageExtensions config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['tsx', 'view.tsx', 'api.tsx', 'api.ts'],
 };
 
 export default nextConfig;
