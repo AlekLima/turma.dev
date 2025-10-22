108135eeaf8b1e4585e4428a33adff7b828840f3
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2025-10-22 09:52:27 +0000

    feat: user page - add grid structure for mobile
    - remove grid from page view by tailwind
    - use css file for setup grid rules

--- src/app/user/page.view.tsx
+++ src/app/user/page.view.tsx
@@ -1,9 +1,11 @@
+import "./styles.css"
+
 export default function UserPage() {
   return (
-    <main className="grid grid-rows-[1fr] grid-cols-[2.25fr_0.25fr_1.50fr_1fr] items-center justify-center my-auto min-h-screen">
-      <div className="left-container flex items-center justify-center h-[300px] text-white" style={{ backgroundColor: 'gray'}}>left content</div>
-      <div className="center-container flex items-center justify-center h-[300px] text-white" style={{ backgroundColor: 'green'}}>center</div>
-      <div className="right-container flex items-center justify-center h-[300px] text-white" style={{ backgroundColor: 'red'}}>left content</div>
+    <main className="user-page-grid grid my-auto min-h-screen">
+      <div className="user-container flex items-center justify-center h-[300px] text-white" style={{ backgroundColor: 'gray'}}>left content</div>
+      <div className="timeline-container flex items-center justify-center h-[300px] text-white" style={{ backgroundColor: 'green'}}>center</div>
+      <div className="content-container flex items-center justify-center h-[300px] text-white" style={{ backgroundColor: 'red'}}>left content</div>
     </main>
   );
 }
