b4f4d36dce85a12bd86e6f868670ad8c1b9b775d
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2025-10-26 20:43:40 +0000

    feat: mock user data

--- src/app/user/page.view.tsx
+++ src/app/user/page.view.tsx
@@ -4,10 +4,21 @@ import "./styles.css"
 
 const listOfYears = [1997, 2016, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025];
 
+const userData = {
+  name: "Jemimma",
+  lastName: "Luz",
+  profilePhotoUrl: "https://github.com/jemluz.png",
+  urls: [
+    "https://github.com/jemluz",
+    "https://www.linkedin.com/in/jemluz",
+    "https://www.turma.dev"
+  ]
+}
+
 export default function UserPage() {
   return (
     <main className="user-page-grid grid my-auto min-h-screen">
-      <User />
+      <User {...userData}/>
       <Timeline years={listOfYears} />
       <div className="content-container flex items-center justify-center h-full">left content</div>
     </main>
