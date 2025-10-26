1968ca1d3868f8fe4376e9c17b3f321cfc7dcefa
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2025-10-26 23:38:06 +0000

    fix: user info grow behavior

--- src/app/user/User/index.tsx
+++ src/app/user/User/index.tsx
@@ -8,34 +8,46 @@ import { ReactNode } from "react";
 import { GoToButtonProps, GoToButtonStyles, GoToButtonType, GoToSectionProps, UserProps } from "./types";
 import { getGoButtonStyles, urlToGoToButtonTypeRegex } from "./utils";
 
-export default function User({ name, lastName, profilePhotoUrl, urls }: UserProps) {
+function RightUserInfo({ name, lastName, profilePhotoUrl, urls}: UserProps) {
   const nameInitials: string = `${name.charAt(0).toUpperCase()}${lastName.charAt(0).toUpperCase()}`
 
   return (
-    <div className="user-container flex items-center justify-end h-full">
-      <div className="flex flex-col w-full items-end justify-center mr-8">
-
-        <div className="flex flex-col items-center pr-8">
-          <Avatar className="w-[80px] h-[80px] mb-4 border-[3px] border-zinc-200">
-            <AvatarImage src={profilePhotoUrl}/>
-            <AvatarFallback>{nameInitials}</AvatarFallback>
-          </Avatar>
-          <h2 className="text-center w-full flex justify-end">
-            {`${name} ${lastName}`}
-          </h2>
-        </div>
-
-        <div className="w-full h-[2px] mb-4 mt-2 pr-8 border-b-[2px] border-b-zinc-200"></div>
-
-        <GoToSection goToUrls={urls}/>
+    <div className="w-fit flex flex-col items-center justify-center mr-8">
+      <div className="flex flex-col items-center">
+        <Avatar className="w-[80px] h-[80px] mb-4 border-[3px] border-zinc-200">
+          <AvatarImage src={profilePhotoUrl}/>
+          <AvatarFallback>{nameInitials}</AvatarFallback>
+        </Avatar>
+        <h2 className="w-full pr-4 text-center flex justify-end text-nowrap">
+          {`${name} ${lastName}`}
+        </h2>
       </div>
+
+      <div className="w-full h-[2px] mb-4 mt-2 border-b-[2px] border-b-zinc-200"></div>
+
+      <GoToSection goToUrls={urls}/>
+    </div>
+  )
+}
+
+export default function User(userData: UserProps) {
+
+  return (
+    <div className="user-container flex items-center justify-end h-full w-full">
+      <div className="w-full flex flex-col items-center justify-center">
+        <div className="h-[120px]"></div>
+        <div className="w-full h-[2px] mb-4 mt-2 border-b-[2px] border-b-zinc-200"></div>
+        <div className="h-[36px]"></div>
+      </div>
+
+      <RightUserInfo {...userData }/>
     </div>
   )
 }
 
 function GoToSection({ goToUrls }: GoToSectionProps) {
   return (
-    <div className="flex gap-2 pr-4">
+    <div className="flex gap-2">
       {goToUrls.map((url: string) => (
         <GoToButton key={url} url={url} />
       ))}
