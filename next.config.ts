14bae661a9b7778d96b32bf5203783b33c7d3f3e
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2025-10-26 01:19:38 +0000

    chore: install phosphor icons and shadcnui avatar component
    - installed @phosphor-icons/react for iconography.
    - added Avatar component from shadcnui for user profile representation
    - updated next.config.ts to optimize package imports

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['tsx', 'view.tsx', 'api.tsx', 'api.ts'],
+  experimental: {
+    optimizePackageImports: ["@phosphor-icons/react"],
+  }
 };
 
 export default nextConfig;
