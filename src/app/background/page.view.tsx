6f747b1506c4c8151703a4506ab6461949f9fac8
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2025-10-29 01:08:57 +0000

    feat: user background mock data and new ContentItem to fill content area

--- src/app/background/page.view.tsx
+++ src/app/background/page.view.tsx
@@ -1,26 +1,33 @@
 import UserInfo from "./UserInfo";
 import Timeline from "./Timeline";
 import "./styles.css"
-
-const listOfYears = [1997, 2016, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025];
-
-const userData = {
-  name: "Jemimma",
-  lastName: "Luz",
-  profilePhotoUrl: "https://github.com/jemluz.png",
-  urls: [
-    "https://github.com/jemluz",
-    "https://www.linkedin.com/in/jemluz",
-    "https://www.turma.dev"
-  ]
-}
+import ContentItem from "./ContentItem";
+import { contentData, listOfYears, userData } from "@/background-data";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function UserPage() {
   return (
     <main className="user-page-grid grid my-auto min-h-screen">
       <UserInfo {...userData}/>
       <Timeline years={listOfYears} />
-      <div className="content-container flex items-center justify-center h-full">left content</div>
+
+      <ScrollArea className="content-container scroll-area flex flex-col max-h-[290px]">
+        {
+          contentData.map(
+            (period, index) => {
+              const isNotUniqueOrLast = contentData.length !== 1 && index < contentData.length - 1;
+
+              return (
+                <ContentItem
+                  key={period.title}
+                  background={period}
+                  isNotUniqueOrLast={isNotUniqueOrLast}
+                />
+              )
+            }
+          )
+        }
+      </ScrollArea>
     </main>
   );
 }
